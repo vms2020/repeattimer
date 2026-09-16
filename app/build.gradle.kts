a33631bd0550a1d06ede2dc74a3f6355903bebc0
seva <vmsk131uh@gmail.com>
2026-09-16 11:26:24 +0300

    workflow script fix3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "io.github.vms2020.repeattimer"
         minSdk = 24
         targetSdk = 37
-        versionCode = 4
-        versionName = "v0.0.4-alpha"
+        versionCode = 5
+        versionName = "v0.0.5-alpha"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
